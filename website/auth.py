8721b9d73935cda92b900c096e7ec7f297cc52cc
DESKTOP-FGQRJE5\titro <titro29@gmail.com>
2024-04-13 17:31:01 +0200

    blueprinty i dzialajaca lokalnie strona

---
+++ website/auth.py
@@ -0,0 +1,20 @@
+"""
+Blueprinty logwania do systemu
+"""
+from flask import Blueprint
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "LOGOWANIE"
+
+
+@auth.route('/logout')
+def logout():
+    return "WYLOGOWANIE"
+
+
+# @auth.route('/signup')
+# def signup():
+#     return "Rejestracja"
---
+++ website/views.py
@@ -0,0 +1,11 @@
+"""
+Blueprinty konkretnych widokow na stonie
+"""
+
+from flask import Blueprint
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def home():
+    return "<h1>SIEMA<h1>"
