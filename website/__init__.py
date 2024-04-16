765f024734c266cc5baad1a11d825374c3ad0d38
DESKTOP-FGQRJE5\titro <titro29@gmail.com>
2024-04-17 01:03:13 +0200

    zmiana do .gitignore i manager logowania

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
+from os import path 
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -15,11 +16,15 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
     
+
     from .views import views
     from .auth import auth
     
-    from . import models
+    from .models import User, Comment
     
     
     app.register_blueprint(views,url_prefix='/')
@@ -28,6 +33,10 @@ def create_app():
     with app.app_context():
         db.create_all()
     
+    
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
     
     return app
 
--- website/auth.py
+++ website/auth.py
@@ -5,19 +5,36 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
+
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
-    data = request.form
-    print(data)
+    if request.method =='POST':
+        email = request.form.get('email') # moze zmienic na indetifikator czy cos
+        password = request.form.get('password')
+        
+        user = User.query.filter_by(email = email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Zalogowano', category='success')
+                login_user(user, remember=True) # remember pamieta ze uzytkownik jest zalogowany ze strony serwera
+                return redirect(url_for('views.home'))
+            else:
+                flash('Złe hasło', category='error')
+        else:
+           flash('Użytkownik nie istnieje', category='error') 
+    
     return render_template("login.html", var="Logowanie",)
 
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return "Wylogowano"
+    logout_user()
+    return redirect(url_for('views.login'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
@@ -29,7 +46,10 @@ def signup():
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
         
-        if password1 != password2:
+        user = User.query.filter_by(email = email).first()
+        if user:
+            flash('Podany email jest przypisany do innego konta', category='error') 
+        elif password1 != password2:
             flash('Hasła muszą być takie same', category='error')
             
         else:
