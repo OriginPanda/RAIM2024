a4e966c3cd6238e63a6abc9a53d39c755afe6cbc
DESKTOP-FGQRJE5\titro <titro29@gmail.com>
2024-04-18 23:03:54 +0200

    zmiana folderu i wyświetlanie obrazków

---
+++ flask-server/website/models.py
@@ -0,0 +1,46 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+from os import path 
+from PIL import Image
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(1000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    data_id = db.Column(db.Integer, db.ForeignKey('medical_data.id'))  
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+    imagepath = db.Column(db.String(1000))
+
+class MedicalData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    patient_id = db.Column(db.Integer, db.ForeignKey('patient.id'))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    comments = db.relationship('Comment')
+    
+    ## Placeholders
+    
+    
+    
+    #TODO dodac reszte danych
+    data = None
+    thumbnail = None
+    category = None
+    
+    
+    # nie wiem co dodać
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    comments = db.relationship('Comment')
+    
+class Patient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50))
+    second_name = db.Column(db.String(50))
+    medicalRecord = db.relationship('MedicalData')
+    
