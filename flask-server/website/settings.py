f02eb523df93d13811f0dd1d9e8b0bc3bed85856
Taras <tarasshupikov@gmail.com>
2024-04-23 09:34:51 +0200

    Dodane moliwosc zmiany danych uzytkownika

--- flask-server/website/settings.py
+++ flask-server/website/settings.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from flask_login import login_required, current_user
+from .models import User
+
+from . import db
 
 
 settings = Blueprint("settings", __name__)
@@ -7,5 +10,13 @@ settings = Blueprint("settings", __name__)
 
 @settings.route('/settings', methods=['GET', 'POST'])
 @login_required
-def patientsettings ():
+def user_settings ():
+    if request.method =='POST':
+        email = current_user.email 
+
+        user = User.query.filter_by(email = email).first()
+        user.name = request.form.get("name")
+        user.email = request.form.get("email")
+        user.password = request.form.get("password")
+
     return render_template("settings.html", user=current_user)
